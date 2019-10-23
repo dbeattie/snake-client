7101befc5157633ad71d035ad513d06a14b248ed
dbeattie <darren@fettle.ca>
2019-10-23 16:47:02 +0000

    refactored connect function into a separate file form play.js

--- play.js
+++ play.js
@@ -1,21 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '172.46.2.204',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.on('close', function() {
-    console.log('you ded');
-  });
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
