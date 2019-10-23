a6a5af474b7f670d1f2d70951d70edaecf8496dd
dbeattie <darren@fettle.ca>
2019-10-23 19:01:40 +0000

    handled user input to leave with ctrl^c

--- play.js
+++ play.js
@@ -1,3 +1,22 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+const handleUserInput = function() {
+  const stdin = process.stdin.on('data', (key) => {
+    if (key === `\u0003`) 
+      process.exit(console.log("Caio!"));
+  })};
+  
+handleUserInput();
+setupInput();
