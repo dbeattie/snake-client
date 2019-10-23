d3e090df2a2de28ce5bf7a94c93dd3199631bdac
dbeattie <darren@fettle.ca>
2019-10-23 19:40:29 +0000

    added the ability to control up/left/down/right to the snake while connected

--- input.js
+++ input.js
@@ -1,21 +1,26 @@
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', handleUserInput);
+  stdin.on('data', (data) => handleUserInput(data, conn));
   return stdin;
 }
-const handleUserInput = function() {
-  const stdin = process.stdin.on('data', (key) => {
-    if (key === `\u0003`) 
+const handleUserInput = function(data, conn) {
+    if (data === `\u0003`) 
       process.exit(console.log("Caio!"));
-  })};
+    if (data === 'w') 
+      conn.write("Move: up");
+    if (data === 'a') 
+      conn.write("Move: left");
+    if (data === 's') 
+      conn.write("Move: down");  
+    if (data === 'd') 
+      conn.write("Move: right");    
+  };
   
 module.exports = {
 setupInput
 }
+
+//w = down, a = left, s = down, and d = right 
