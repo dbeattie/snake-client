7e281644f7bde699394a284dbc73110b4da8c559
dbeattie <darren@fettle.ca>
2019-10-23 19:16:56 +0000

    moved last function into a separate input file

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-const handleUserInput = function() {
-  const stdin = process.stdin.on('data', (key) => {
-    if (key === `\u0003`) 
-      process.exit(console.log("Caio!"));
-  })};
-  
-handleUserInput();
+
 setupInput();
