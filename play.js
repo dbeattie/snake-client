c04f154f689f9cff660d0dcc2f1d1971bf094255
dbeattie <darren@fettle.ca>
2019-12-04 20:32:13 +0000

    added handleUserInput to new input file

--- play.js
+++ play.js
@@ -1,7 +1,9 @@
 // play.js
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 connect();
+setupInput();
 
 // DON'T KNOW WHAT THIS WAS PREVIOUSLY FROM...
 // const { connect } = require('./client');
