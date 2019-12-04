3eee318784a68d63e411033df3d7f6671e136f97
dbeattie <darren@fettle.ca>
2019-12-04 21:03:00 +0000

    added controls to the snake

--- input.js
+++ input.js
@@ -1,32 +1,20 @@
-const handleUserInput = function(data) {
-  if (data === `\u0003`) 
-    process.exit(console.log("Caio!"));
-  // if (data === 'w') 
-  //   conn.write("Move: up");
-  // if (data === 'a') 
-  //   conn.write("Move: left");
-  // if (data === 's') 
-  //   conn.write("Move: down");  
-  // if (data === 'd') 
-  //   conn.write("Move: right");
-  // if (data === 'y')
-  //   conn.write("Say: Yo");
-  // if (data === 't')
-  //   conn.write("Say: d-train");     
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (data) => handleUserInput(data));
-  return stdin;
-}
-
-
-
-
+// const handleUserInput = function(data, conn) {
+//   if (data === `\u0003`) 
+//     process.exit(console.log("Caio!"));
+//   if (data === 'w') 
+//     //console.log('up');
+//     conn.write("Move: up");
+//   if (data === 'a') 
+//     conn.write("Move: left");
+//   if (data === 's') 
+//     conn.write("Move: down");  
+//   if (data === 'd') 
+//     conn.write("Move: right");
+//   if (data === 'y')
+//     conn.write("Say: Yo");
+//   if (data === 't')
+//     conn.write("Say: d-train");     
+// };
 
 // const setupInput = function(conn) {
 //   const stdin = process.stdin;
@@ -36,22 +24,31 @@ const setupInput = function() {
 //   stdin.on('data', (data) => handleUserInput(data, conn));
 //   return stdin;
 // }
-// const handleUserInput = function(data, conn) {
-//     if (data === `\u0003`) 
-//       process.exit(console.log("Caio!"));
-//     if (data === 'w') 
-//       conn.write("Move: up");
-//     if (data === 'a') 
-//       conn.write("Move: left");
-//     if (data === 's') 
-//       conn.write("Move: down");  
-//     if (data === 'd') 
-//       conn.write("Move: right");
-//     if (data === 'y')
-//       conn.write("Say: Yo");
-//     if (data === 't')
-//       conn.write("Say: d-train");     
-//   };
+
+const setupInput = function(conn) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (data) => handleUserInput(data, conn));
+  return stdin;
+}
+const handleUserInput = function(data, conn) {
+    if (data === `\u0003`) 
+      process.exit(console.log("Caio!"));
+    if (data === 'w') 
+      conn.write("Move: up");
+    if (data === 'a') 
+      conn.write("Move: left");
+    if (data === 's') 
+      conn.write("Move: down");  
+    if (data === 'd') 
+      conn.write("Move: right");
+    if (data === 'y')
+      conn.write("Say: Yo");
+    if (data === 't')
+      conn.write("Say: d-train");     
+  };
   
 module.exports = {
 setupInput
--- play.js
+++ play.js
@@ -1,14 +1,16 @@
 // play.js
-const { connect } = require('./client');
-const { setupInput } = require('./input');
-console.log('Connecting ...');
-connect();
-setupInput();
-
-// DON'T KNOW WHAT THIS WAS PREVIOUSLY FROM...
 // const { connect } = require('./client');
 // const { setupInput } = require('./input');
 
 // console.log('Connecting ...');
-// const conn = connect();
+// connect();
+
 // setupInput(conn);
+
+// OLD CODE WORKED BETTER FROM...
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
+console.log('Connecting ...');
+const conn = connect();
+setupInput(conn);
